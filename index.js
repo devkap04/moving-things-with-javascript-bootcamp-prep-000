1b079bd3cbe2aa67890dde4c9d00025532c1d731
devinkapelanski <devinkapelanski@yahoo.com>
2018-10-19 04:19:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '');
+  var right = parseInt(rightNumbers, 10);
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+})
